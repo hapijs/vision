606b77a0e7b6c756d3b0f6601a07c97530a10220
William S. Woodruff <williamswoodruff@gmail.com>
2017-11-08 21:31:58 -0500

    Update cms example

--- examples/cms/server.js
+++ examples/cms/server.js
@@ -3,89 +3,113 @@
 
 const Path = require('path');
 const Hapi = require('hapi');
-const Pages = require('./pages');
+const PagesClass = require('./pages')
 const Vision = require('../..');
+const Handlebars = require('handlebars');
 
+const Pages = new PagesClass(`${__dirname}/_pages`);
 
 // Declare internals
 
 const internals = {};
 
+const today = new Date();
+const thisYear = today.getFullYear();
 
-const view = function (viewName) {
 
-    return function (request, reply) {
+const getTplArgs = (argsToAppend) => {
 
-        return reply.view(viewName, { title: viewName });
+    console.log('thisYear', thisYear);
+    return Object.assign(
+        {},
+        argsToAppend,
+        { year: thisYear }
+    );
+};
+
+
+const getViewSimpleHandler = (viewName) => {
+
+    return (request, h) => {
+
+        return h.view(viewName, getTplArgs({ title: viewName }));
     };
 };
 
 
-const getPages = function (request, reply) {
+const getPages = (request, h) => {
 
-    return reply.view('index', { pages: Object.keys(Pages.getAll()), title: 'All pages' });
+    return h.view('index', getTplArgs({
+        pages: Object.keys(Pages.getAll()),
+        title: 'All pages'
+    }));
 };
 
 
-const getPage = function (request, reply) {
+const getPage = (request, h) => {
 
-    return reply.view('page', { page: Pages.getPage(request.params.page), title: request.params.page });
+    return h.view('page', getTplArgs({
+        page: Pages.getPage(request.params.page),
+        title: request.params.page
+    }));
 };
 
 
-const createPage = function (request, reply) {
+const createPage = (request, h) => {
 
     Pages.savePage(request.payload.name, request.payload.contents);
-    return reply.view('page', { page: Pages.getPage(request.payload.name), title: 'Create page' });
+    return h.view('page', getTplArgs({
+        page: Pages.getPage(request.payload.name),
+        title: 'Create page'
+    }));
 };
 
 
-const showEditForm = function (request, reply) {
+const showEditForm = (request, h) => {
 
-    return reply.view('edit', { page: Pages.getPage(request.params.page), title: 'Edit: ' + request.params.page });
+    return h.view('edit', getTplArgs({
+        page: Pages.getPage(request.params.page),
+        title: 'Edit: ' + request.params.page
+    }));
 };
 
 
-const updatePage = function (request, reply) {
+const updatePage = (request, h) => {
 
     Pages.savePage(request.params.page, request.payload.contents);
-    return reply.view('page', { page: Pages.getPage(request.params.page), title: request.params.page });
+    return h.view('page', getTplArgs({
+        page: Pages.getPage(request.params.page),
+        title: request.params.page
+    }));
 };
 
 
-internals.main = function () {
+internals.provision = async () => {
 
-    const server = new Hapi.Server();
-    server.connection({ port: 8000, state: { ignoreErrors: true } });
-    server.register(Vision, (err) => {
-
-        if (err) {
-            throw err;
-        }
-
-        server.views({
-            engines: { html: require('handlebars') },
-            path: Path.join(__dirname, 'views'),
-            layout: true,
-            partialsPath: Path.join(__dirname, 'views', 'partials')
-        });
-
-        server.route({ method: 'GET', path: '/', handler: getPages });
-        server.route({ method: 'GET', path: '/pages/{page}', handler: getPage });
-        server.route({ method: 'GET', path: '/create', handler: view('create') });
-        server.route({ method: 'POST', path: '/create', handler: createPage });
-        server.route({ method: 'GET', path: '/pages/{page}/edit', handler: showEditForm });
-        server.route({ method: 'POST', path: '/pages/{page}/edit', handler: updatePage });
-        server.start((err) => {
-
-            if (err) {
-                throw err;
-            }
-
-            console.log('Server is listening at ' + server.info.uri);
-        });
+    const server = Hapi.Server({
+        port: 3000,
+        state: { ignoreErrors: true }
     });
+
+    await server.register(Vision);
+
+    server.views({
+        engines: { html: Handlebars },
+        path: Path.join(__dirname, 'views'),
+        layout: true,
+        partialsPath: Path.join(__dirname, 'views', 'partials')
+    });
+
+    server.route({ method: 'GET', path: '/', handler: getPages });
+    server.route({ method: 'GET', path: '/pages/{page}', handler: getPage });
+    server.route({ method: 'GET', path: '/create', handler: getViewSimpleHandler('create') });
+    server.route({ method: 'POST', path: '/create', handler: createPage });
+    server.route({ method: 'GET', path: '/pages/{page}/edit', handler: showEditForm });
+    server.route({ method: 'POST', path: '/pages/{page}/edit', handler: updatePage });
+
+    await server.start();
+    console.log('Server is listening at ' + server.info.uri);
 };
 
 
-internals.main();
+internals.provision();
